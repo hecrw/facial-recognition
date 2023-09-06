a4e0fa4a8e0c1ad76b4aa0b71a7d276ca0b01cfd
hecrw <136959713+hecrw@users.noreply.github.com>
2023-09-07 00:10:39 +0300

    Update detector.py

--- face_recognizer/training/detector.py
+++ face_recognizer/training/detector.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import face_recognition as fr
 import pickle
 from collections import Counter
+from PIL import Image, ImageDraw, ImageFont
 
 location = "output/encodings.pkl"
 def encode_faces(model = "hog"):
@@ -26,11 +27,34 @@ def recognize_faces(image_location, model = "hog"):
     input_image = fr.load_image_file(image_location)
     input_face_location = fr.face_locations(input_image, model=model)
     input_face_encod = fr.face_encodings(input_image, input_face_location)
+    
+    pillow_image = Image.fromarray(input_image)
+    draw = ImageDraw.Draw(pillow_image)
     for box, encoding in zip(input_face_location, input_face_encod):
         name = _recognize_face(encoding, loaded_encodings)
         if not name:
             name = "unknown"
+        display_face(draw, box, name)
+    del draw
+    pillow_image.show()
         
 def _recognize_face(encoding, loaded_encodings):
     bool_matches = fr.compare_faces(loaded_encodings["encodings"], encoding)
+    votes = Counter(name for match, name in zip(bool_matches,loaded_encodings["names"]) if match)
+    if votes:
+        return votes.most_common(1)[0][0]
+    
+def display_face(draw, bounding_box, name):
+    top, right, bottom, left = bounding_box
+    draw.rectangle(((left, top), (right, bottom)), outline="blue", width=4)
+    text_left, text_top, text_right, text_bottom = draw.textbbox((left, bottom), name)
+    font = ImageFont.truetype("training/pixel.ttf",24)
+    draw.text((text_left, text_top),name,fill="white",font=font)
+    
+# def validate():
+#     for file in Path("validation").rglob("*"):
+#         if file.is_file():
+#             recognize_faces(str(file.absolute()))
+    
 # encode_faces()
+# validate()
